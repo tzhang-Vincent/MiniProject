088f166a8d501d9f51f7bce98747acb82433f74f
Zhang Ting <zhangting.199612@gmail.com>
2019-09-22 17:34:49 -0400

    syntax correct

--- Google API/sentiment.py
+++ Google API/sentiment.py
@@ -14,10 +14,10 @@ import six
 # Analyze the sentiment of each tweet text and assign them the corresponding score
 def sentiment_analysis(text):
 	document = language.types.Document(
-        content=text,
-        type=language.enums.Document.Type.PLAIN_TEXT)
+		content=text,
+		type=language.enums.Document.Type.PLAIN_TEXT)
 	language_client=language.LanguageServiceClient()
 	result=language_client.analyze_sentiment(document)
 	score = result.document_sentiment.score
-    magnitude = result.document_sentiment.magnitude
-    return score,magnitude
+	magnitude = result.document_sentiment.magnitude
+	return score,magnitude
